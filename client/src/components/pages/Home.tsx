32d17733e801669e0d9c34d7ae02c49970c51c96
SusieHatter <susiivaldez@gmail.com>
2022-06-13 13:58:04 -0500

    fix white text on white bg on home.tsx

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -3,7 +3,7 @@ import { DISCORD_LOGIN_URL } from "../../api/links";
 function HomePage() {
   return (
     <div className="bg-slate-50 w-full h-auto lg:w-1/3  m-auto rounded-md p-4 flex flex-col items-center">
-      <h2 className="text-dark text-2xl pb-4">Continue with Discord</h2>
+      <h2 className="text-black text-2xl pb-4">Continue with Discord</h2>
       <a
         href={DISCORD_LOGIN_URL}
         className="bg-blurple p-3 rounded-md text-lg hover:bg-dark-blurple active:brightness-90"
